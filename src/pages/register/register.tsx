09f23b2858d3ab5a7563c3c68eb008118f613d1e
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-08-01 11:15:35 +0800

    add: register

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,40 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Form, Input, Button } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import './register.css'
+import React, { useState } from 'react';
 
-const LoginForm = () => {
-    const onFinish = (values: any) => {
-        console.log('Received values of form: ', values);
+type SlideVerifyProps = {
+    onVerified: () => void;
+};
+
+const SlideVerify: React.FC<SlideVerifyProps> = ({ onVerified }) => {
+    // 这只是一个简单的例子，你可能需要一个更复杂、更安全的滑动验证组件
+    const [isVerified, setVerified] = useState(false);
+
+    const handleSlide = () => {
+        setVerified(true);
+        onVerified();
     };
 
     return (
-        <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-        >
-            <Form.Item
-                name="username"
-                rules={[{ required: true, message: 'Please input your Username!' }]}
-            >
-                <Input prefix={<UserOutlined />} placeholder="Username" />
-            </Form.Item>
-            <Form.Item
-                name="password"
-                rules={[{ required: true, message: 'Please input your Password!' }]}
-            >
-                <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                    Log in
-                </Button>
-            </Form.Item>
-        </Form>
-
+        <div onPointerUp={handleSlide}>
+            滑动验证{isVerified ? '通过' : '未通过'}
+        </div>
     );
 };
-export default LoginForm
+
+const RegistrationPage: React.FC = () => {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [verified, setVerified] = useState(false);
+
+    const handleVerification = () => {
+        setVerified(true);
+    };
+
+    const handlePhoneNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPhoneNumber(event.target.value);
+    };
+
+    const handleGetCodeClick = () => {
+        if (verified) {
+            // 在这里调用发送验证码的API
+            console.log(`发送验证码到 ${phoneNumber}`);
+        }
+    };
+
+    return (
+        <div>
+            <input
+                type="tel"
+                placeholder="手机号"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+            />
+            <SlideVerify onVerified={handleVerification} />
+            <button onClick={handleGetCodeClick} disabled={!verified}>
+                获取验证码
+            </button>
+        </div>
+    );
+};
+
+export default RegistrationPage;
