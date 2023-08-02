f4162bc05e92eb59eff7a0e338f00fccfeb16e06
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-08-02 15:02:23 +0800

    add: node

--- src/pages/demo/staging/demo.tsx
+++ src/pages/demo/staging/demo.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import TodoList from "./03_TodoList/todoList";
+import Proxy from "./04_proxy/proxy";
 export default function DemoStag() {
     return (
         <>
-            <br /><span>-------------</span>
+            <br /><span>04-代理服务器案例-------------</span>
+            <Proxy/>
+            <br /><span>03-TODOList案例-------------</span>
             <TodoList />
         </>
     )
